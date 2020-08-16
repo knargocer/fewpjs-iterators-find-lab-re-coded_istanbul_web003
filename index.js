1c55c334633908497f620d4857eede14cc1b2c7b
Knar <knargr@gmail.com>
2020-08-16 14:24:16 +0300

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,16 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+function superbowlWin(array){
+  const win = array.find(obj=>obj.result ==="W");
+  // if( win=== NaN){
+  //   return undefined;
+  // }
+  if(win){
+  return win.year;
+  }
+  else{
+    return undefined;
+  }
+  
+}
